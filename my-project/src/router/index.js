dbcbf4772d31f2ed6c7816a6a2204ade5822108f
Pablo Gonzalez <alu0100887037@ull.edu.es>
2019-11-21 19:42:44 +0000

    Coneccion con base de datos 1

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -4,16 +4,16 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: () => import('@/components/HelloWorld.vue')
-    },
-    {
-      path: '/authentication',
-      name: 'authentication',
-      component: () => import('@/components/Authentication.vue')
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'HelloWorld',
+            component: () => import('@/components/HelloWorld.vue')
+        },
+        {
+            path: '/authentication',
+            name: 'authentication',
+            component: () => import('@/components/Authentication.vue')
+        }
+    ]
 })
