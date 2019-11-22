90b8c3d9cb70af4b32a14cb9fa428bb0f66b74dc
Pablo Gonzalez <alu0100887037@ull.edu.es>
2019-11-22 10:05:37 +0000

    Reestructuracion [2]

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -4,7 +4,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 import App from "./App";
-import router from "./router";
+import router from "./_router";
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
