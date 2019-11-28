416553e64967b1cd2bf208a3562ce775eef82291
Pablo Gonzalez <alu0100887037@ull.edu.es>
2019-11-28 18:13:29 +0000

    Archivo jwt.js [2]

---
+++ my-project/src/_helpers/jwt.js
@@ -0,0 +1,28 @@
+const expressJwt = require('express-jwt');
+const config = require('config.json')
+const userService = require('../components/Registro')
+
+module.exports = jwt;
+
+function jwt() {
+    const secret = config.secret;
+    return expressJwt({ secret, isRevoked }).unless({
+        path: [
+            // public routes that don't require authentication
+            '../components/Home',
+            '../component/Registro',
+            '../componentes/InicioSesion'
+        ]
+    });
+}
+
+async function isRevoked(req, payload, done) {
+    const user = await userService.getById(payload.sub);
+
+    // revoke token if user no longer exists
+    if (!user) {
+        return done(null, true);
+    }
+
+    done();
+};
