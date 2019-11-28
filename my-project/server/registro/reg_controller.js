d81173b792d9789fa29437259e2257df65baa6a1
Pablo Gonzalez <alu0100887037@ull.edu.es>
2019-11-28 21:01:11 +0000

    Borrar archivos obsoletos

---
+++ my-project/server/registro/reg_controller.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const userService = require('./user.service');
+
+// routes
+router.post('/authenticate', authenticate);
+router.post('/register', register);
+router.get('/', getAll);
+router.get('/current', getCurrent);
+router.get('/:id', getById);
+router.put('/:id', update);
+router.delete('/:id', _delete);
