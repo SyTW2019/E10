d071f6e4ffa18f1a171dc7a223faf15ab0f46f15
alu0100893601-Sergio <alu0100893601@ull.edu.es>
2019-11-29 16:54:29 +0000

    Reviviendo la API REST

--- my-project/src/_services/user_service.js
+++ my-project/src/_services/user_service.js
@@ -27,7 +27,8 @@ function login(username, password) {
         .then(user => {
             // login successful if there's a jwt token in the response
             if (user.token) {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                // store user details and jwt token in local storage to keep 
+                // user logged in between page refreshes
                 localStorage.setItem('user', JSON.stringify(user));
             }
 
