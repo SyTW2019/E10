485a1e565127e089fb6fdace8536d77ad869a49b
Pablo Gonzalez Gonzalez <pablogonzalez@MacBook-Pro-de-Pablo.local>
2020-08-17 11:12:26 +0100

    Reseteo del proyecto

--- src/_helpers/index.js
+++
@@ -1,6 +0,0 @@
-// The helpers index file groups all helper exports together so they can be imported in other parts 
-// of the app using only the folder path, and enables importing multiple helpers in a single statement
-
-//export * from './fake-backend';
-export { router } from './router';
-export * from './auth_header';
