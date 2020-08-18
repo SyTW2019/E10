cd4f40c7f18b559b278842d51f38bb1db064ebf6
alu0100893601-Sergio <alu0100893601@ull.edu.es>
2020-08-18 19:59:30 +0100

    Poniendo cosas del antiguo

---
+++ src/helpers/router.js
@@ -0,0 +1,51 @@
+// The vue router defines all of the routes for the application, and contains a function that runs before
+// each route change to prevent unauthenticated users from accessing restricted routes.
+
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('@/components/Home/Home.vue')
+    },
+    {
+      path: '/registro',
+      name: 'registro',
+      component: () => import('@/components/Registro/Registro.vue')
+    },
+    {
+      path: '/iniciosesion',
+      name: 'iniciosesion',
+      component: () => import('@/components/InicioSesion/InicioSesion.vue')
+    },
+    {
+      path: '/calendario',
+      name: 'calendario',
+      component: () => import('@/components/Calendario/Calendario.vue')
+    },
+    {
+      path: '/perfil',
+      name: 'perfil',
+      component: () => import('@/components/Perfil/Perfil.vue')
+    },
+    { path: '*', redirect: '/' }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/perfil', '/calendario', '/iniciosesion', '/registro', '/'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    return next('/iniciosesion');
+  }
+
+  next();
+})
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App.vue";
+import BootstrapVue from "bootstrap-vue";
+import VeeValidate from "vee-validate";
+import { router } from "./helpers/router";
+import { store } from "./store";
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(VeeValidate);
 
 new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+	el: "#app",
+	router,
+	store,
+	components: { App },
+	template: "<App/>",
+});
