8bae2764be085f74b4e011d765fccdea33e2054f
Pablo Gonzalez Gonzalez <alu0100887037@ull.edu.es>
2020-08-21 18:02:47 +0100

    db.js primera version

---
+++ server/helpers/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/test", { useNewUrlParser: true})
+    .then(db => console.log('DB conectada'))
+    .catch(err => console.error(err));
+
+module.exports = {
+    User: require('../users/user-model')
+}
