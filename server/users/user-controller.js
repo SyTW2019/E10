33669a5803d02bb48fa8d0cb8f2026ce80904da8
SergioDelgadoLopez <sergio.delglo@gmail.com>
2020-08-21 22:50:15 +0100

    [10]

--- server/users/user-controller.js
+++ server/users/user-controller.js
@@ -1,55 +1,68 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userService = require('./user-service');
+const userService = require("./user-service");
 
-router.post('/iniciosesion', authenticate);
-router.post('/registro', register);
-router.get('/', getAll);
-router.get('/current', getCurrent);
-router.get('/:id', getById);
-router.put('/:id', update);
-router.delete('/:id', );
+router.post("/iniciosesion", authenticate);
+router.post("/registro", register);
+router.get("/", getAll);
+router.get("/current", getCurrent);
+router.get("/:id", getById);
+router.put("/:id", update);
+router.delete("/:id");
 
 module.exports = router;
 
 function authenticate(req, res, next) {
-    userService.authenticate(req.body)
-        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Usuario o contraseña incorrecta '}))
-        .catch(err => next(err));
+    userService
+        .authenticate(req.body)
+        .then((user) =>
+            user
+                ? res.json(user)
+                : res
+                      .status(400)
+                      .json({ message: "Usuario o contraseña incorrecta " })
+        )
+        .catch((err) => next(err));
 }
 
 function register(req, res, next) {
-    userService.create(req.body)
+    userService
+        .create(req.body)
         .then(() => res.json({}))
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }
 
 function getAll(req, res, next) {
-    userService.getAll()
+    userService
+        .getAll()
         .then(() => res.json(users))
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }
 
 function getCurrent(req, res, next) {
-    userService.getById(req.user.sub)
-        .then(user => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
+    userService
+        .getById(req.user.sub)
+        .then((user) => (user ? res.json(user) : res.sendStatus(404)))
+        .catch((err) => next(err));
 }
 
 function getById(req, res, next) {
-    userService.getById(req.params.id)
-        .then(user => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
+    userService
+        .getById(req.params.id)
+        .then((user) => (user ? res.json(user) : res.sendStatus(404)))
+        .catch((err) => next(err));
 }
 
 function update(req, res, next) {
-    userService.update(req.params.id, req.body)
+    userService
+        .update(req.params.id, req.body)
         .then(() => res.json({}))
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }
 
 function _delete(req, res, next) {
-    userService.delete(req.params.id)
+    userService
+        .delete(req.params.id)
         .then(() => res.json({}))
-        .catch(err => next(err));
+        .catch((err) => next(err));
 }
